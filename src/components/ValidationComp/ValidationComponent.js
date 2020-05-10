7c0e3c8a37ab0c9c4bd407a2e15f6d4d38b23c0f
nicolasraffray <nraffray@gmail.com>
2020-05-10 15:49:51 +0100

    third task done now conditionally rendering text

--- src/components/ValidationComp/ValidationComponent.js
+++ src/components/ValidationComp/ValidationComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const ValidationComponent = (props) => {
+  let returnString = <p>Text too Short</p>;
+
+  if(props.tLength > 5){
+    returnString = <p>Text is Long Enough</p>
+  }
   return (
     <div>
       <p>
         The length of the pararagraph is: {props.tLength} Characters
       </p>
+      {returnString}
     </div>
   )
 }
